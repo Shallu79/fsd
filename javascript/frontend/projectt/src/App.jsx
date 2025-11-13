c72497a6ac74955421fbeb897c79988eec0754c9
Shalini Srivastava <shalinisri249@gmail.com>
2025-11-13 09:46:10 +0530

    hello

---
+++ javascript/frontend/projectt/src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import Fashion from './components/Fashion';
+import './App.css';
+
+function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(res => res.json())
+      .then(data => setProducts(data))
+  }, []);
+
+  return (
+    <div className="fashion-container">
+      {products.map((item, i) => (
+        <Fashion
+          key={i}
+          title={item.title}
+          price={`$${item.price}`}
+          image={item.image}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default App;
